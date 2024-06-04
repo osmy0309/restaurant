9475bd824e9e13d5131e3052b12d50e3cb9b8ab5
osmy0309 <osmy0309@gmail.com>
2024-06-05 00:58:22 +0200

    correccion de imagenes en servicios menu

--- src/component/cards/CardServiceMenu.tsx
+++ src/component/cards/CardServiceMenu.tsx
@@ -10,13 +10,12 @@ function CardServiceMenu({ image, title, description, id }: CardMenuProps) {
 		<div
 			className={`w-full h-auto p-5 flex bg-transparent z-10 items-center bg-white  hover:shadow-left-bottom transition-shadow duration-300 rounded-[20px] cursor-pointer `}
 		>
-			<div className={`w-[25%] h-[100px] bg-[url('${image}')] bg-cover bg-center !rounded-full flex justify-center items-center my-[1rem]`}></div>
-			<div className=" w-[75%]  flex flex-col gap-3 font-Roboto ] rounded-b-[20px] pl-3">
-				<div className="text-[24px] font-normal font-Sail_Regular text-center text-[#1F0B01]">{title}</div>
-				<div
-					className=" text-[13px] font-Roboto text-[#888888] text-justify flex justify-center pb-4"
-					dangerouslySetInnerHTML={{ __html: description }}
-				/>
+			<div className={`w-[30%] h-full  !rounded-full flex justify-center items-center my-[1rem]`}>
+				<img className="w-[100%] h-full rounded-t-[20px]" src={`${image}`} />
+			</div>
+			<div className=" w-[70%]  flex flex-col gap-3 font-Roboto ] rounded-b-[20px] pl-3">
+				<div className="text-[24px] font-normal font-Sail_Regular text-[#1F0B01]">{title}</div>
+				<div className=" text-[13px] font-Roboto text-[#888888] text-justify flex  pb-4" dangerouslySetInnerHTML={{ __html: description }} />
 			</div>
 		</div>
 	);
