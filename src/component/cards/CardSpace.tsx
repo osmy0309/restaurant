53762723678da75d3afa77e7d7a09ec47f24f89a
roge <rogeidis9007@gmail.com>
2024-06-05 09:44:04 -0400

    fix(footer): contact seccion update

--- src/component/cards/CardSpace.tsx
+++ src/component/cards/CardSpace.tsx
@@ -18,7 +18,7 @@ function CardSpace({ image, title, description,category }: CardMenuProps) {
 			<div className="flex flex-col p-[36px] bg-white gap-3  rounded-b-[20px] ">
 				<div className="text-[16px] font-bold  text-[#525252]">{category}</div>
 				<div className="font-bold  font-Sail_Regular text-[#1F0B01] text-[40px]">{title}</div>
-				<div className=" text-[13px] text-[#888888] font-Roboto"> {description}</div>
+				<div className=" text-[13px] text-[#888888] font-Roboto" dangerouslySetInnerHTML={{ __html: description }}/>
 			</div>
 
 			<div className="flex p-[36px] gap-[1rem]">
