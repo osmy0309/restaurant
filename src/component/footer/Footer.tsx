19c9eb6993aebc6f0d431cac714097188069c65d
roge <rogeidis9007@gmail.com>
2024-06-05 09:24:44 -0400

    fix(footer): resolv render error

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -8,14 +8,14 @@ function Footer() {
 			<div className="basis-1/5"/>
 			<div className="basis-2/5">
 			<article className="text-wrap h-[200px]">
-  <h3 className="text-[18px] font-bold h-[2rem] text-[#E38A5D]">{setting.footer?.aboutUs?.name}</h3>
-  <p className="text-[14px] text-[#fff]">{setting.footer?.aboutUs?.description.substring(0, 500)}</p>
+  <h3 className="text-[18px] font-bold h-[2rem] text-[#E38A5D]">{setting?.footer?.aboutUs?.name}</h3>
+  <p className="text-[14px] text-[#fff]">{setting?.footer?.aboutUs?.description.substring(0, 500)}</p>
 </article>
 			</div>
   			<div className="basis-3/10 columns-2">
 			  <article className="text-wrap h-[200px]">
   <h3 className="text-[18px] font-bold h-[2rem] text-[#E38A5D]">Reserva</h3>
-  {setting.footer?.booking?.map(b=>(
+  {setting?.footer?.booking?.map(b=>(
 	<div className="hover:cursor-pointer hover:text-[20px]">
 		<a className="text-[14px] text-[#fff]" href={b.url}>{b.name}</a>
 	</div>	
@@ -37,7 +37,7 @@ function Footer() {
   <h3 className="text-[18px] font-bold h-[2rem] text-[#E38A5D]"> Sigue a:</h3>
   <div className="columns-3">
   {
-	setting.footer?.followUs?.map(f=>(
+	setting?.footer?.followUs?.map(f=>(
 		<div>
 			<p className="text-[14px] text-[#fff]">{`${f.name}`}</p>
 			<div className="columns-3">
