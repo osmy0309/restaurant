0be264b24dcd57396be16828d64ef822e18aeef6
osmy0309 <osmy0309@gmail.com>
2024-06-11 19:25:04 +0200

    arreglo de links cards espacios home

--- src/component/IconNetwork.tsx
+++ src/component/IconNetwork.tsx
@@ -1,22 +1,19 @@
-import { Link } from "react-router-dom"
-import { SocialNetworkData } from "../shared/dtos/settingsDTO"
+import { SocialNetworkData } from "../shared/dtos/settingsDTO";
 
 interface IconNetworkProps {
-    network:SocialNetworkData
+	network: SocialNetworkData;
 }
 
-const IconNetwork = ({network}:IconNetworkProps) =>{
-    console.log("Valores en las redes :",network);
-    
-    return (
-        <>
-        <Link to={network.value || "/"}  target="_blank" rel="noopener noreferrer" className="z-50">
-        {
-            <img className="h-[30px] hover:cursor-pointer" src={`/images/card/${network.name.toLowerCase()}.png`} />
-        }
-        </Link>
-        </>
-    )
-}
+const IconNetwork = ({ network }: IconNetworkProps) => {
+	console.log("Valores en las redes :", network);
+
+	return (
+		<>
+			<a className="z-50" href={network.value} target="_blank">
+				{<img className="h-[30px] hover:cursor-pointer" src={`/images/card/${network.name.toLowerCase()}.png`} />}
+			</a>
+		</>
+	);
+};
 
 export default IconNetwork;
--- src/component/cards/CardSpace.tsx
+++ src/component/cards/CardSpace.tsx
@@ -8,35 +8,43 @@ interface CardMenuProps {
 	description: string;
 	id: number;
 	category: string;
-	networks:SocialNetworkData[];
+	networks: SocialNetworkData[];
 }
 
-function CardSpace({ image, title, description, category,id,networks }: CardMenuProps) {
+function CardSpace({ image, title, description, category, id, networks }: CardMenuProps) {
+	const handleSocialLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+		e.stopPropagation();
+	};
 	return (
 		<Link to={`/spaces/${id}`}>
-		<div
-			className={`h-full flex flex-col rounded-[20px] bg-transparent border  z-10 hover:hover:shadow-left-bottom transition-shadow duration-300 border-bordermenu `}
-		>
-			<div className="w-[100%] bg-transparent ">
-				<img className="w-[100%] h-[250px] rounded-t-[20px]" src={`${image}`} />
-			</div>
-
 			<div
-				className="flex flex-col p-[36px] bg-white gap-3  rounded-b-[20px] max-h-[25rem] h-[25rem] overflow-auto "
-				style={{ scrollbarWidth: "none", scrollbarColor: "#888888 #f4f4f4" }}
+				className={`h-full flex flex-col rounded-[20px] bg-transparent border relative z-10 hover:hover:shadow-left-bottom transition-shadow duration-300 border-bordermenu `}
 			>
-				<div className="text-[16px] font-bold  text-[#525252]">{category ? category : <br />}</div>
-				<div className="font-bold  font-Sail_Regular text-[#1F0B01] text-[40px]">{title}</div>
-				<div
-					className="max-h-[20rem] h-[20rem] overflow-auto text-[13px] text-[#888888] font-Roboto"
-					dangerouslySetInnerHTML={{ __html: description }}
-				/>
-			</div>
+				<div className="w-[100%] bg-transparent ">
+					<img className="w-[100%] h-[250px] rounded-t-[20px]" src={`${image}`} />
+				</div>
 
-			<div className="flex p-[36px] gap-[1rem]">
-				{networks?.length > 0 && networks.map(n=><IconNetwork network={n}/>)}
+				<div
+					className="flex flex-col p-[36px] bg-white gap-3  rounded-b-[20px] max-h-[25rem] h-[25rem] overflow-auto "
+					style={{ scrollbarWidth: "none", scrollbarColor: "#888888 #f4f4f4" }}
+				>
+					<div className="text-[16px] font-bold  text-[#525252]">{category ? category : <br />}</div>
+					<div className="font-bold  font-Sail_Regular text-[#1F0B01] text-[40px]">{title}</div>
+					<div
+						className="max-h-[20rem] h-[20rem] overflow-auto text-[13px] text-[#888888] font-Roboto"
+						dangerouslySetInnerHTML={{ __html: description }}
+					/>
+				</div>
+
+				<div className="absolute  -bottom-4 z-50 flex p-[36px] gap-[1rem] justify-center items-center">
+					{networks?.length > 0 &&
+						networks.map((n) => (
+							<a key={n.name} className="z-50" href={n.value} target="_blank" onClick={handleSocialLinkClick} rel="noopener noreferrer">
+								{<img className="h-[30px] hover:cursor-pointer hover:h-[40px] hover:rounded-full" src={`/images/card/${n.name.toLowerCase()}.png`} />}
+							</a>
+						))}
+				</div>
 			</div>
-		</div>
 		</Link>
 	);
 }
